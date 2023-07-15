d7047d9d8df75105e8b09b07f1ce9d526522c07e
Avner Levy <66301432+Avner152@users.noreply.github.com>
2023-07-15 23:31:48 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const itemRouter = require("./routes/Item");
+
 const cors = require("cors");
 require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
 
-const db = require("./models");
+// const db = require("./models");
 
 // Routers
-const postProduct = require("./routes/Product");
-app.use("/products", postProduct);
+app.use("/items", itemRouter);
 
 const PORT = process.env.PORT || 5001;
-
-db.sequelize
-  .sync()
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
       console.log("Server is running on port: ", PORT);
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log("bla");
+    console.log(err);
+  });
 
 // console.log("avner");
